491e4c37ee1c2b8784c9f77c434346dae4ce7db0
Marcos <marcos@nuvemshop.com.br>
2020-03-12 20:48:04 -0300

    Back configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,18 +16,29 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `Good night sweet heart`,
-    //     short_name: `GNSH`,
-    //     start_url: `/`,
-    //     background_color: `#ffffff`,
-    //     theme_color: `#ffffff`,
-    //     display: `minimal-ui`,
-    //     //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Good night sweet heart`,
+        short_name: `GNSH`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `minimal-ui`,
+        //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-XXXXXXXX-X`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-postcss`,
       options: {
