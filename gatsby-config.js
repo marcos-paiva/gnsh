01aae54ee2a313826ff5552096d579f7b353cca0
Marcos <marcos@nuvemshop.com.br>
2020-03-12 17:02:47 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,18 +16,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Good night sweet heart`,
-        short_name: `GNSH`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#ffffff`,
-        display: `minimal-ui`,
-        //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `Good night sweet heart`,
+    //     short_name: `GNSH`,
+    //     start_url: `/`,
+    //     background_color: `#ffffff`,
+    //     theme_color: `#ffffff`,
+    //     display: `minimal-ui`,
+    //     //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-postcss`,
       options: {
