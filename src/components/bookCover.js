309bc55600e74d4ab74b597a2f55672b81b6f6dd
Marcos <marcos@nuvemshop.com.br>
2020-03-14 23:34:56 -0300

    Add image to bookCover component

--- src/components/bookCover.js
+++ src/components/bookCover.js
@@ -2,12 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import covers from '../styles/modules/bookCover.module.css'
 import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 
 const bookCover = ({title, category, readtime, image, excerpt, url}) => {
     return (
         <Link to={url} className={covers.item}>
             <h2>{title}</h2>
             <p>{excerpt}</p>
+            <Img fluid={image} alt={title}/>
         </Link>
     )
 }
