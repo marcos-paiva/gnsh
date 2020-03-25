be270be6a05a9f28467f0788ccb4624546bba93c
Marcos <marcos@nuvemshop.com.br>
2020-03-24 23:40:06 -0300

    Create taleShare.js

---
+++ src/components/taleShare.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from "prop-types"
+import Article from '../styles/modules/article.module.css'
+import GirlShare from '../static/persons/girl-sharing.inline.svg'
+
+const TaleShare = ({time, topics}) => {
+    return(
+        <section className={Article.data}>
+            <section className={Article.dataImage}>
+            <GirlShare/>
+            </section>
+            <section className={Article.dataContent}>
+                <h5>Antes de ir, compartilhe essa história com a sua rede</h5>
+            </section>
+        </section>
+    )
+}
+
+TaleShare.propTypes = {
+
+}
+
+TaleShare.defaultProps = {
+
+}
+
+export default TaleShare
