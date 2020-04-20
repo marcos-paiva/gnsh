e5074d2db777a3a7767e2a81f72cb93fbd3eae12
Marcos <marcos@nuvemshop.com.br>
2020-04-20 10:15:48 -0300

    update node to fetch tales taxonomy

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require(`path`)
+const __ = require(`lodash`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -17,24 +18,50 @@ exports.createPages = async({graphql, actions}) => {
     const { createPage } = actions
     const result = await graphql(`
         query {
-            allMarkdownRemark {
-            edges {
-                node {
-                fields {
-                    slug
-                }
+            talesRemark: allMarkdownRemark (
+                sort: { order: DESC, fields: [frontmatter___date] }
+                limit: 2000
+            ) {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                        frontmatter {
+                            tags
+                        }
+                    }
                 }
             }
+            tagsRemark: allMarkdownRemark (limit: 2000) {
+                group(field: frontmatter___tags) {
+                    fieldValue
+                }
             }
         }
     `)
 
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+
+    const tales = result.data.talesRemark.edges
+
+    tales.forEach(({ node }) => {
         createPage({
             path: node.fields.slug,
             component: path.resolve(`./src/pages/content.js`),
             context: {
                 slug: node.fields.slug,
+            },
+        })
+    })
+
+    const tags = result.data.tagsRemark.group
+
+    tags.forEach(tag => {
+        createPage({
+            path: `/tema/${__.kebabCase(tag.fieldValue)}/`,
+            component: path.resolve(`./src/pages/category.js`),
+            context: {
+                tag: tag.fieldValue,
             },
         })
     })
