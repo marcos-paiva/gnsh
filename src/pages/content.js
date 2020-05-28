34615fa1383f670bd8aa9c469d92f8b52b6fb21a
Marcos <marcos@nuvemshop.com.br>
2020-05-27 21:02:02 -0300

    Update content.js

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
-import Header from '../components/header'
-import TaleData from '../components/taleData'
-import TaleShare from '../components/taleShare'
+import React from "react"
+import { graphql } from "gatsby"
 
-import '../styles/variables.css'
-import Article from '../styles/modules/article.module.css'
+// Internal components
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Header from "../components/header"
+import TaleData from "../components/taleData"
+import TaleShare from "../components/taleShare"
+
+// Styles
+import "../styles/variables.css"
+import Article from "../styles/modules/article.module.css"
 
 export default ({ data }) => {
   const post = data.markdownRemark
-  const image = post.frontmatter.featured ? post.frontmatter.featured.childImageSharp.resize : null
+  const image = post.frontmatter.featured
+    ? post.frontmatter.featured.childImageSharp.resize
+    : null
 
-  return(
+  return (
     <Layout>
       <Seo
         title={post.frontmatter.title}
@@ -24,61 +29,65 @@ export default ({ data }) => {
       <Header />
 
       <article className={Article.global}>
-
         <header className={Article.header}>
           <h1 className={Article.title}>{post.frontmatter.title}</h1>
-          <TaleData time={post.timeToRead}/>
+          <TaleData time={post.timeToRead} />
         </header>
 
-        <div dangerouslySetInnerHTML={{ __html: post.html }} className={Article.container} />
+        <div
+          dangerouslySetInnerHTML={{ __html: post.html }}
+          className={Article.container}
+        />
 
         <footer>
-          <p><strong>Fonte:</strong> {post.frontmatter.source}</p>
+          <p>
+            <strong>Fonte:</strong> {post.frontmatter.source}
+          </p>
         </footer>
-        <TaleShare/>
+        <TaleShare />
       </article>
     </Layout>
   )
 }
 
 export const query = graphql`
-    query($slug: String){
-        site {
-            siteMetadata {
-              title
-              author
+  query($slug: String) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      timeToRead
+      excerpt(pruneLength: 160)
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+        categories
+        source
+        featured {
+          childImageSharp {
+            resize(width: 1200, cropFocus: CENTER, quality: 80) {
+              src
+              width
+              height
             }
-          }
-        markdownRemark(fields: { slug: { eq: $slug } }) {
-          html
-          timeToRead
-          excerpt(pruneLength: 160)
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            categories
-            source
-            featured {
-              childImageSharp {
-                resize(width: 1200, cropFocus: CENTER, quality: 80) {
-                  src
-                  width
-                  height
-                }
-                resolutions {
-                  srcWebp
-                  src
-                }
-                sizes {
-                  src
-                  srcWebp
-                  srcSet
-                }
-              }
+            resolutions {
+              srcWebp
+              src
+            }
+            sizes {
+              src
+              srcWebp
+              srcSet
             }
           }
         }
+      }
     }
+  }
 `
