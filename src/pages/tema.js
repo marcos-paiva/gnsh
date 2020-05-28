7e09d3caf1134293d74b328190b97e821ce82651
Marcos <marcos@nuvemshop.com.br>
2020-05-27 21:02:20 -0300

    Update tema.js

--- src/pages/tema.js
+++ src/pages/tema.js
@@ -8,70 +8,68 @@ import kebabCase from "lodash/kebabCase"
 import { Link, graphql } from "gatsby"
 
 // Internal components
-import Layout from '../components/layout'
-import Seo from '../components/seo'
-import Header from '../components/header'
-import Cover from '../components/catCover'
-
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Header from "../components/header"
+import Cover from "../components/catCover"
 
 const TagsPage = ({
-
-    data: {
-        allMarkdownRemark: { group },
-        site: {
-            siteMetadata: { title },
-        },
+  data: {
+    allMarkdownRemark: { group },
+    site: {
+      siteMetadata: { title },
     },
-    }) => (
-    <Layout>
-        <Seo title={title} />
-        <Header title="Temas"/>
-        <ul className="book-wrapper">
-            {group.map( tag => (
-            <li key={tag.fieldValue}>
-                <Cover
-                    title={tag.fieldValue}
-                    readtime={tag.totalCount}
-                    url={`/tema/${kebabCase(tag.fieldValue)}/`}
-                />
-            </li>
-            ))}
-        </ul>
-    </Layout>
+  },
+}) => (
+  <Layout>
+    <Seo title={title} />
+    <Header title="Temas" />
+    <ul className="book-wrapper">
+      {group.map(tag => (
+        <li key={tag.fieldValue}>
+          <Cover
+            title={tag.fieldValue}
+            readtime={tag.totalCount}
+            url={`/tema/${kebabCase(tag.fieldValue)}/`}
+          />
+        </li>
+      ))}
+    </ul>
+  </Layout>
 )
 
 TagsPage.propTypes = {
-    data: PropTypes.shape({
-        allMarkdownRemark: PropTypes.shape({
-            group: PropTypes.arrayOf(
-            PropTypes.shape({
-                fieldValue: PropTypes.string.isRequired,
-                totalCount: PropTypes.number.isRequired,
-            }).isRequired
-            ),
-        }),
-        site: PropTypes.shape({
-            siteMetadata: PropTypes.shape({
-                title: PropTypes.string.isRequired,
-            }),
-        }),
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      group: PropTypes.arrayOf(
+        PropTypes.shape({
+          fieldValue: PropTypes.string.isRequired,
+          totalCount: PropTypes.number.isRequired,
+        }).isRequired
+      ),
     }),
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+      }),
+    }),
+  }),
 }
 
 export default TagsPage
 
 export const pageQuery = graphql`
-    query {
-        site {
-            siteMetadata {
-                title
-            }
-        }
-        allMarkdownRemark(limit: 2000) {
-            group(field: frontmatter___tags) {
-              fieldValue
-              totalCount
-          }
-        }
+  query {
+    site {
+      siteMetadata {
+        title
+      }
     }
+    allMarkdownRemark(limit: 2000) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
 `
