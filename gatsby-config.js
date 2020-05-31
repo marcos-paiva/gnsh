b419bd43ccd9a2d11f1491a100e5b72346e73f10
Marcos <marcos@nuvemshop.com.br>
2020-05-30 21:38:48 -0300

    Improve config

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,10 +45,10 @@ module.exports = {
         name: `Good night sweet heart`,
         short_name: `GNSH`,
         start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#ffffff`,
+        background_color: `#FCF4F2`,
+        theme_color: `#FCF4F2`,
         display: `minimal-ui`,
-        //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/static/icon.png`,
       },
     },
     {
