78039f4705336313b997865869af74c06f7e523a
Marcos <marcos@nuvemshop.com.br>
2020-06-02 09:05:00 -0300

    Trying to get nodes from tags

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,14 +14,11 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     }
 }
 
-exports.createPages = async({graphql, actions}) => {
+exports.createPages = async({graphql, actions, reporter}) => {
     const { createPage } = actions
     const result = await graphql(`
-        query {
-            talesRemark: allMarkdownRemark (
-                sort: { order: DESC, fields: [frontmatter___date] }
-                limit: 2000
-            ) {
+        query myQuery {
+            talesRemark: allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, limit: 2000) {
                 edges {
                     node {
                         fields {
@@ -37,15 +34,18 @@ exports.createPages = async({graphql, actions}) => {
                 group(field: frontmatter___tags) {
                     fieldValue
                     nodes {
-                        fields {
-                            slug
-                        }
+                        id
                     }
                 }
             }
         }
     `)
 
+    // handle errors
+    if (result.errors) {
+        reporter.panicOnBuild(`Error while running GraphQL query.`)
+        return
+    }
 
     const tales = result.data.talesRemark.edges
 
@@ -60,25 +60,16 @@ exports.createPages = async({graphql, actions}) => {
     })
 
     const tags = result.data.tagsRemark.group
-    const contentTags = result.data.tagsRemark.group.nodes
     const postsPerPage = 6
 
-    console.log(contentTags)
-
-    //const numPages = Math.ceil(contentTags.length / postsPerPage)
-
-    // path: i === 0 ? `/blog` : `/blog/${i + 1}`
-    // Array.from({ length: numPages }).forEach((_, i) => {
-
-    tags.forEach((_, i, tag) => {
+    tags.forEach( (tag) => {
+    //Array.from({ length: postsPerPage }).forEach( (_, i, tag) => {
         createPage({
-            path: `/tema/${ i === 0 ? __.kebabCase(tag.fieldValue) : __.kebabCase(tag.fieldValue)}/${i + 1}/ }`,
+            path: `/tema/${__.kebabCase(tag.fieldValue)}/`,
             component: path.resolve(`./src/pages/category.js`),
             context: {
-                tag: tag.fieldValue,
-                limit: postsPerPage,
-                skip: i * postsPerPage
-            },
+                tag: tag.fieldValue
+            }
         })
     })
 }
