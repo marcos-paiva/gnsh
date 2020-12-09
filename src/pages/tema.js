cd4f30d170f1df218e8f8fcbd54eb69b1a3cf989
Marcos <marcos@nuvemshop.com.br>
2020-12-09 03:21:49 -0300

    Re-naming files

--- src/pages/tema.js
+++ src/pages/tema.js
@@ -1,75 +1,86 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-// Utilities
-import kebabCase from "lodash/kebabCase"
-
 // Components
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 // Internal components
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Header from "../components/header"
-import Cover from "../components/catCover"
+import Cover from "../components/bookCover"
 
-const TagsPage = ({
-  data: {
-    allMarkdownRemark: { group },
-    site: {
-      siteMetadata: { title },
-    },
-  },
-}) => (
-  <Layout>
-    <Seo title={title} />
-    <Header title="Temas" />
-    <ul className="book-wrapper">
-      {group.map(tag => (
-        <li key={tag.fieldValue}>
-          <Cover
-            title={tag.fieldValue}
-            readtime={tag.totalCount}
-            url={`/tema/${kebabCase(tag.fieldValue)}/`}
-          />
-        </li>
-      ))}
-    </ul>
-  </Layout>
-)
+const Tags = ({ pageContext, data }) => {
+    const { tag } = pageContext
+    const { edges, totalCount } = data.allMarkdownRemark
+    const tagHeader = `${totalCount} história${totalCount === 1 ? "" : "s"} sobre ${tag}`
 
-TagsPage.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      group: PropTypes.arrayOf(
-        PropTypes.shape({
-          fieldValue: PropTypes.string.isRequired,
-          totalCount: PropTypes.number.isRequired,
-        }).isRequired
-      ),
-    }),
-    site: PropTypes.shape({
-      siteMetadata: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-      }),
-    }),
-  }),
+    return (
+    <Layout>
+        <Seo title={tagHeader} />
+        <Header title={tagHeader} />
+        <Link to="/tema/" className="link">Todos os temas</Link>
+        <ul className="book-wrapper">
+        {edges.map(({ node }) => (
+            <li key={node.fields.slug}>
+            <Cover
+                title={node.frontmatter.title}
+                readtime={node.timeToRead}
+                excerpt={node.excerpt}
+                url={node.fields.slug}
+                theme={node.frontmatter.theme}
+            />
+            </li>
+        ))}
+        </ul>
+    </Layout>
+    )
 }
 
-export default TagsPage
+Tags.propTypes = {
+    pageContext: PropTypes.shape({
+        tag: PropTypes.string.isRequired,
+    }),
+    data: PropTypes.shape({
+        allMarkdownRemark: PropTypes.shape({
+            totalCount: PropTypes.number.isRequired,
+            edges: PropTypes.arrayOf(
+                PropTypes.shape({
+                    node: PropTypes.shape({
+                        frontmatter: PropTypes.shape({
+                            title: PropTypes.string.isRequired,
+                        }),
+                        fields: PropTypes.shape({
+                            slug: PropTypes.string.isRequired,
+                        }),
+                    }),
+                }).isRequired
+            ),
+        }),
+    }),
+}
+
+export default Tags
 
 export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(limit: 2000) {
-      group(field: frontmatter___tags) {
-        fieldValue
+    query getTheme($tag: String) {
+        allMarkdownRemark(
+            sort: { fields: [frontmatter___date], order: DESC }
+            filter: { frontmatter: { tags: { in: [$tag] } } }
+        ) {
         totalCount
-      }
+            edges {
+                node {
+                    fields {
+                        slug
+                    }
+                    frontmatter {
+                        title
+                        theme
+                    }
+                    timeToRead
+                }
+            }
+        }
     }
-  }
 `
---
+++ src/pages/temas.js
@@ -0,0 +1,77 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+// Utilities
+import kebabCase from "lodash/kebabCase"
+
+// Components
+import { graphql } from "gatsby"
+
+// Internal components
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Header from "../components/header"
+import Cover from "../components/catCover"
+
+const TagsPage = ({
+    data: {
+        allMarkdownRemark: { group },
+        site: {
+            siteMetadata: { title },
+        },
+    },
+    }) => (
+    <Layout>
+
+        <Seo title={title} />
+        <Header title="Temas" />
+        <ul className="book-wrapper">
+            {group.map(tag => (
+                <li key={tag.fieldValue}>
+                    <Cover
+                    title={tag.fieldValue}
+                    readtime={tag.totalCount}
+                    url={`/temas/${kebabCase(tag.fieldValue)}/`}
+                    />
+                </li>
+            ))}
+        </ul>
+    </Layout>
+    )
+    console.log(TagsPage)
+
+TagsPage.propTypes = {
+    data: PropTypes.shape({
+        allMarkdownRemark: PropTypes.shape({
+            group: PropTypes.arrayOf(
+            PropTypes.shape({
+                fieldValue: PropTypes.string.isRequired,
+                totalCount: PropTypes.number.isRequired,
+            }).isRequired
+            ),
+        }),
+        site: PropTypes.shape({
+            siteMetadata: PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            }),
+        }),
+    }),
+}
+
+export default TagsPage
+
+export const pageQuery = graphql`
+    query {
+        site {
+            siteMetadata {
+            title
+            }
+        }
+        allMarkdownRemark(limit: 2000) {
+            group(field: frontmatter___tags) {
+            fieldValue
+            totalCount
+            }
+        }
+    }
+`
